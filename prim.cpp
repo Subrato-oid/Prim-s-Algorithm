5ceac8f7c0e2dd434e27a6738f600fb08d8c4d59
Subrato-oid <56046542+Subrato-oid@users.noreply.github.com>
2020-04-25 22:22:57 +0530

    Add files via upload

---
+++ prim.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<climits>
+#define I INT_MAX
+
+using namespace std;
+
+ int cost[8][8]={{I,I,I,I,I,I,I,I},
+               {I,I,25,I,I,I,5,I},
+               {I,25,I,12,I,I,I,10},
+               {I,I,12,I,8,I,I,I},
+               {I,I,I,8,I,16,I,14},
+               {I,I,I,I,16,I,20,18},
+               {I,5,I,I,I,20,I,I},
+               {I,I,10,I,14,18,I,I}};
+
+int near[8]={I,I,I,I,I,I,I,I};
+
+int t[2][6];
+int main(){
+
+    int i,j,k,u,v,n=7,min=I;
+
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=n;j++){
+            if(cost[i][j]<min){
+                min=cost[i][j];
+                u=i;
+                v=j;
+            }
+        }
+    }
+    t[0][0]=u;
+    t[1][0]=v;
+    near[u]=near[v]=0;
+
+    for(int j=1;j<n;j++){
+        if(near!=0){
+            if(cost[j][u]<cost[j][v])
+                near[j]=u;
+            else
+                near[j]=v;
+        }
+    }
+    for(int j=1;j<n-1;j++){
+        min=I;
+        for(i=1;i<n;i++){
+          if(near[i]!=0 && cost[i][near[i]] < min){
+              min=k;
+              min=cost[i][near[i]];
+          }
+        }
+
+        t[0][i]=k;
+        t[1][i]=near[k];
+        near[k]=0;
+
+        for(int i=1;i<n;i++){
+            if(near[i]!=0 && cost[i][k] < cost[i][near[i]]){
+                near[i]=k;
+            }
+        }
+    }
+    for(i=0;i<n-1;i++){
+        cout<<"("<<t[0][i]<<","<<t[1][i]<<")"<<endl;
+    }
+    return 0;
+}
+
